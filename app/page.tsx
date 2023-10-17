5bd3e309714459823825def6776aa411bec72be2
Somebody <lighthing111@gmail.com>
2023-10-17 17:22:18 +0300

    ✅ Fixed align text

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,9 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center lg:justify-center">
       <motion.div
-        initial={{ y: -200, opacity: 0}}
+        initial={{ y: -100, opacity: 0}}
         animate={{ y: 0, opacity: 1 }}
+        className="flex flex-col items-center"
       >
         <h1 className="mt-[4rem] text-[2rem] font-bold lg:-mt-[2.45rem]">
           Our Pricing
